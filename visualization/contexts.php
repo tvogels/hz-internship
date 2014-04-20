cede0a98723d927d89570b934d9324fd8435aca8
Thijs Vogels <t.vogels@me.com>
2014-04-20 21:56:53 +0200

    Rename files

---
+++ visualization/contexts.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+  <title>Create ElasticSearch Index</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/css/csspinner.css">
+</head>
+
+<body>
+
+  <div class="container">
+
+    <h1>Contexts <small>Internship HzBwNature</small></h1>
+
+  </div>
+
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+  <script src="/bower_components/handlebars/handlebars.min.js"></script>
+  <script src="/bower_components/elasticsearch/elasticsearch.jquery.min.js"></script>
+  <script src="/js/contexts.js"></script>
+
+</body>
+
+</html>
